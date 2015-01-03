4c5fa56a93d7b8094b98a1c6f54454f4f2ae818d
bghtyu <bghtyu123@gmail.com>
2015-01-03 17:48:55 +0800

    change passwd

--- mail.py
+++ mail.py
@@ -7,12 +7,12 @@ from email.mime.text import MIMEText
 import getpass
 
 # Prompt the user for connection info
-to_email = 'bghtyu123@yeah.net'
+to_email = 'ich@agsay.com'
 servername = 'smtp.agsay.com'
 username = 'so@agsay.com'
-password = 'www163com'
+password = 'qazwsx'
 
-r = requests.get('http://jwc.xjtu.edu.cn/html/tzgg/3.html')
+r = requests.get('http://jwc.xjtu.edu.cn/html/tzgg/1.html')
 print r.status_code
 print r.headers
 print r.encoding
